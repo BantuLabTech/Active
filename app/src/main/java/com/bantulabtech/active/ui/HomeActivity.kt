267f8dbad5251e2b58614cc0722879b86048d340
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-02-17 13:09:18 +0200

    Hide drawer, bottom navigation

--- app/src/main/java/com/bantulabtech/active/ui/HomeActivity.kt
+++ app/src/main/java/com/bantulabtech/active/ui/HomeActivity.kt
@@ -1,44 +1,67 @@
 package com.bantulabtech.active.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bantulabtech.active.R
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     lateinit var navController: NavController
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var drawerNavigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_home)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val drawerNavView: NavigationView = findViewById(R.id.drawer_nav_view)
+        bottomNavigationView = findViewById(R.id.nav_view)
+        drawerNavigationView = findViewById(R.id.drawer_nav_view)
+        drawerLayout = findViewById(R.id.container)
 
         navController = findNavController(R.id.home_nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.loginFragment, R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
-
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        drawerNavView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+        drawerNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_,destination,_ ->
+            hideBottomNavBar(destination)
+            hideDrawerNavBar(destination)
+        }
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-//        navController.popBackStack()
+    fun hideBottomNavBar(destination: NavDestination){
+        if(destination.id == R.id.loginFragment){
+            bottomNavigationView.visibility = View.GONE
+        }
+        else{
+            bottomNavigationView.visibility = View.VISIBLE
+        }
+    }
 
+    fun hideDrawerNavBar(destination: NavDestination){
+        if(destination.id == R.id.loginFragment){
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        }
+        else{
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        }
     }
 }
